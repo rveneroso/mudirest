dfa4d454d3a0e74b6f4941856a93464d20b735f7
Renato Veneroso <rveneroso@gmail.com>
2022-01-19 18:41:36 -0300

    Contém diversas alterações. Teoricamente é o último commit do dia 19/01/2022

---
+++ src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
@@ -0,0 +1,43 @@
+package br.com.alura.mvc.mudi.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovaOferta;
+import br.com.alura.mvc.mudi.model.Oferta;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/ofertas")
+public class OfertasRest {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	
+	@PostMapping
+	public Oferta criaOferta(RequisicaoNovaOferta requisicao) {
+		
+		Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoId());
+		if(!pedidoBuscado.isPresent()) {
+			return null;
+		}
+		
+		Pedido pedido = pedidoBuscado.get();
+		
+		Oferta novaOferta = requisicao.toOferta();
+		novaOferta.setPedido(pedido);
+		pedido.getOfertas().add(novaOferta);
+		// Quando o pedido for salvo, por causa do Cascade.ALL automaticamente a Oferta será salva
+		pedidoRepository.save(pedido);
+		return novaOferta;
+		
+		
+	}
+
+}
---
+++ src/main/java/br/com/alura/mvc/mudi/api/PedidosRest.java
@@ -0,0 +1,36 @@
+package br.com.alura.mvc.mudi.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidosRest {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("aguardando")
+	public List<Pedido> getPedidosAguardandoOfertas() {
+		
+		Sort sort = Sort.by("id").descending();
+		// O primeiro parâmetro é a página inicial. O índice 0 corresponde à página 1.
+		PageRequest paginacao = PageRequest.of(0, 2, sort);
+		
+		List<Pedido> pedidosPendentes = pedidoRepository.findByStatus(StatusPedido.AGUARDANDO, paginacao );
+		
+		return pedidosPendentes;
+
+	}
+
+}
