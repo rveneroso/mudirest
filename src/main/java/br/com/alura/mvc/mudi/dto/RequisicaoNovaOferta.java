49c555a2ad1c9364951bf87e642912ab88b3f428
Renato Veneroso <rveneroso@gmail.com>
2022-01-20 12:12:50 -0300

    Mudei de ideia: peguei o código disponbilizado pelo instrutor e fiz os ajustes necessários no meu código. Nesse commit a validação está funcionando

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import br.com.alura.mvc.mudi.model.Oferta;
 
 public class RequisicaoNovaOferta {
@@ -11,7 +14,13 @@ public class RequisicaoNovaOferta {
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	private Long pedidoId;
+	
+	@NotNull
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")
 	private String valor;
+	
+	@NotNull
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
 	private String dataDaEntrega;
 	private String comentario;
 	
